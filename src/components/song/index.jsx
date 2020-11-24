97f934436061d2a9256b44c742028d172be6fd78
pavel97107 <pavel97107@gmail.com>
2020-11-24 18:46:02 +0300

    addSongComponent

---
+++ src/components/song/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    
+    return (
+        <div className="song-container">
+            <h1>Picture</h1>
+            <h1>Song name</h1>
+            <h1>Artist</h1>
+        </div>
+    )
+}
